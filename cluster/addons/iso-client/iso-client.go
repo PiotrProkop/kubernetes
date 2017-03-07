2042ce0a2d507bc1fdc10ef9cfca585a5946be3c
PiotrProkop <piotr.prokop@intel.com>
2017-03-07 16:30:39 +0100

    Adding comments and TODOs

--- cluster/addons/iso-client/iso-client.go
+++ cluster/addons/iso-client/iso-client.go
@@ -17,9 +17,12 @@ import (
 )
 
 const (
+	// kubelet eventDispatcher address
 	eventDispatcherAddress = "localhost:5433"
-	eventHandlerAddress    = "localhost:5444"
-	name                   = "iso"
+	// iso-client own address
+	eventHandlerAddress = "localhost:5444"
+	// name of isolator
+	name = "iso"
 )
 
 type EventHandler interface {
@@ -29,6 +32,7 @@ type EventHandler interface {
 type eventHandler struct {
 }
 
+// starting eventHandler grpc server
 func (e *eventHandler) Start(socketAddress string) {
 	glog.Infof("Starting eventHandler")
 	lis, err := net.Listen("tcp", socketAddress)
@@ -36,51 +40,62 @@ func (e *eventHandler) Start(socketAddress string) {
 		glog.Fatalf("failed to bind to socket address: %v", err)
 	}
 	s := grpc.NewServer()
+
 	lifecycle.RegisterEventHandlerServer(s, e)
+
 	if err := s.Serve(lis); err != nil {
 		glog.Fatalf("failed to start event handler server: %v", err)
 	}
 }
+
+// TODO: implement PostStop
 func (e *eventHandler) Notify(context context.Context, event *lifecycle.Event) (reply *lifecycle.EventReply, err error) {
-	if event.Kind == 0 {
-		glog.Infof("Received PreStop event with such payload: %v\n", event.CgroupInfo)
+	switch event.Kind {
+	case lifecycle.Event_POD_PRE_START:
+		glog.Infof("Received PreStart event with such payload: %v\n", event.CgroupInfo)
+		// Pinning created pod to static 0-1 core
 		path := fmt.Sprintf("%s%s%s", "/sys/fs/cgroup/cpuset", event.CgroupInfo.Path, "/cpuset.cpus")
 		glog.Infof("Our path: %v", path)
 		err := ioutil.WriteFile(path, []byte("0-1"), 0644)
 		if err != nil {
-			glog.Fatalf("OOOOPS: %v", err)
+			return nil, fmt.Errorf("Ooops: %v", err)
 		}
 
-	} else {
+		return &lifecycle.EventReply{
+			Error:      "",
+			CgroupInfo: event.CgroupInfo,
+		}, nil
+
+	default:
 		return nil, fmt.Errorf("Wrong event type")
 	}
-	return &lifecycle.EventReply{
-		Error:      "",
-		CgroupInfo: event.CgroupInfo,
-	}, nil
 
 }
 
+// TODO: split it to smaller functions
 func main() {
 	flag.Parse()
 	glog.Info("Staring ...")
-	ctx := context.Background()
 	cxn, err := grpc.Dial(eventDispatcherAddress, grpc.WithInsecure())
 	if err != nil {
 		glog.Fatalf("failed to connect to eventDispatcher: %v", err)
 	}
 	client := lifecycle.NewEventDispatcherClient(cxn)
 	glog.Infof("Registering handler: %s\n", name)
+
 	registerToken := string(uuid.NewUUID())
 	registerRequest := &lifecycle.RegisterRequest{
 		SocketAddress: eventHandlerAddress,
 		Name:          name,
 		Token:         registerToken,
 	}
+
+	ctx := context.Background()
 	reply, err := client.Register(ctx, registerRequest)
 	if err != nil {
 		glog.Fatalf("Failed to register handler: %v")
 	}
+
 	glog.Infof("Registered iso: %v\n", reply)
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -96,8 +111,9 @@ func main() {
 		}
 		glog.Infof("Unregistered iso: %v\n", rep)
 
-		os.Exit(1)
+		os.Exit(0)
 	}()
+	// TODO: Move it to separete goroutine to avoid races and start it before registering
 	server := &eventHandler{}
 	server.Start(eventHandlerAddress)
 
